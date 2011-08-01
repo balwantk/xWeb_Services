6480840991c8d02dde742103a19bae6781cef72a
Balwant Kane <balwant.kane@xipcraft.com>
2011-08-01 12:25:07 +0530

    Added RailsApp

---
+++ xWeb_Services_Rails_App/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XWebServicesRailsApp::Application.config.secret_token = 'a3ce75d34ae92fdff74e6ff62102b6cb332d70de55249bb47f97c1531462b34183c38c5b30859c99840230b439ea4193d81832329eba02ea815591b7f2095a79'
---
+++ xWeb_Services_Rails_App/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+XWebServicesRailsApp::Application.config.session_store :cookie_store, :key => '_xWeb_Services_Rails_App_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# XWebServicesRailsApp::Application.config.session_store :active_record_store
