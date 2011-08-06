4d389d0f4401f6576d264ac434da5b4ccfa2d455
Balwant Kane <balwant.kane@xipcraft.com>
2011-08-06 13:00:27 +0530

    Setup asset pipelines and finalized Spec and BDD app structure

--- xWeb_Services_Rails_App/config/environment.rb
+++ xWeb_Services_Rails_App/config/environment.rb
@@ -4,9 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 XWebServicesRailsApp::Application.initialize!
 
-Sass::Plugin.options[:style] = :compact
-Sass::Plugin.options[:syntax] = :scss
-Sass::Plugin.options[:template_location] = './app'
-Sass::Plugin.options[:css_location] = './public/bin'
-
-puts "SASS SETTINGS DONE!!!! \n:D:D:D:D:D"
+# Sass::Plugin.options[:style] = :compact
+# Sass::Plugin.options[:syntax] = :scss
+# Sass::Plugin.options[:template_location] = './app'
+# Sass::Plugin.options[:css_location] = './public/bin'
+# 
+# puts "SASS SETTINGS DONE!!!! \n:D:D:D:D:D"
