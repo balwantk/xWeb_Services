4fa75856ea442bbb55eda3d8d9fc5ae44dad3d5d
Balwant Kane <balwant.kane@xipcraft.com>
2011-08-26 11:51:28 +0530

    Moved old specs and code to the Stash folder

--- xWeb_Services_Rails_App/Stash/message_mailer_spec.rb
+++ xWeb_Services_Rails_App/Stash/message_mailer_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe MessageMailer do
   it "should send email with attributes described in the message hash"
-  it "should throw error on failuer"
+  it "should throw error on failure"
   it "should acknowledge on successful sent"
   
 end
