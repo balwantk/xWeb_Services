040f95c5b1a3d249b47f7e18608de3e2bfcfed47
Balwant Kane <balwant.kane@xipcraft.com>
2011-08-29 18:58:50 +0530

    Tweaked around line spacing

--- xWeb_Services_Rails_App/public/javascripts/global_helpers/x_check.js
+++ xWeb_Services_Rails_App/public/javascripts/global_helpers/x_check.js
@@ -1,6 +1,5 @@
 var xCheck = {
-  forJavaScript:function(document){
-
+  forJavaScript:function(){
     if(/\bno-js\b/.test(document.getElementsByTagName("html")[0].className)) {
       document.getElementsByTagName("html")[0].className = document.getElementsByTagName("html")[0].className.replace(/\bno-js\b/, '');
     }
