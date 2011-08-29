8638e5c065947c33a1d247ecd25e5f3254557b44
Balwant Kane <balwant.kane@xipcraft.com>
2011-08-29 16:59:51 +0530

    Cross browser and adaptive testing done without JS

--- xWeb_Services_Rails_App/spec/javascripts/sub_apps/xipcraft_static_site_spec.js
+++ xWeb_Services_Rails_App/spec/javascripts/sub_apps/xipcraft_static_site_spec.js
@@ -12,23 +12,4 @@ describe("General javascript that powers the Xipcraft static site", function() {
       */
     });
   });
-  
-  describe("Google analytics code", function() {
-    it("should be defined", function() {
-      expect(_gag).toBeDefined();
-    });
-    
-    it("should have the correct account number", function() {
-      /*
-        TODO @dinesh
-        A/c number is UA-25360290-1
-      */
-    });
-    
-    describe("the correct settings for the _gag array", function() {
-      /*
-        TODO @dinesh 
-      */
-    });
-  });
 });
