6eee1c0fc7d11e616da896d07fa668f40c324b80
Balwant Kane <balwant.kane@xipcraft.com>
2011-08-29 12:36:21 +0530

    Added test stubs

--- xWeb_Services_Rails_App/spec/javascripts/global_helpers/x_check_spec.js
+++ xWeb_Services_Rails_App/spec/javascripts/global_helpers/x_check_spec.js
@@ -1,15 +1,30 @@
 describe("Global checker function that's inspired from Modernizr", function() {
+
+  
   it("should be defined", function() {
     expect(xCheck).toBeDefined();
   });
   
   describe("checker for detecting javascript on a page", function() {
+    
+    beforeEach(function() {
+      /*
+        TODO @dinesh  SETUP ENV
+                      - Expect var htmlEl = document.documentElement.getElementByTagName("html") is defined
+                      - Check if htmlEl.className has string "no-js"
+                      - If no then append the sting "no-js" to the className
+                      - Expect htmlEl.className has string "no-js" to be truthy
+      */
+    });
+
+    
     it("should be defined", function() {
       expect(xCheck.forJavaScript).toBeDefined();
     });
-    it("should replace the html class 'no-js' with 'js' if js is active", function() {
+    it("should replace all the html tags ka class 'no-js' with 'js' if js is active", function() {
       /*
-        TODO @dinesh
+        TODO @dinesh RUN TEST> xCheck.forJavaScript();
+                     WRITE EXPECTATION> Expect htmlEl.className has string "no-js" to be falsy
       */
     });
   });
